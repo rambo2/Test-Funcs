70fc3f34324c25d44c2bdfb728336bf3142e95b3
Rambo <ctony123@gmail.com>
2022-01-25 09:49:36 +1000

    Add files via upload

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Install nginx
-apt-get -y update
-apt -y install nginx
+sudo apt-get -y update
+sudo apt -y install nginx
 
 # Remove the default page
-rm /var/www/html/index.nginx-debian.html
+sudo rm /var/www/html/index.nginx-debian.html
 
 # Create the default page for the region
-wget https://raw.githubusercontent.com/rambo2/Test-Funcs/master/index.$1.html -O /var/www/html/index.html
+sudo wget https://raw.githubusercontent.com/rambo2/Test-Funcs/master/index.$1.html -O /var/www/html/index.html
