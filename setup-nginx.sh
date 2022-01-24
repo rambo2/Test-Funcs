6c4ed9c471544a28b429663d273e9d959c71c8ae
Rambo <ctony123@gmail.com>
2022-01-25 07:47:42 +1000

    Add files via upload

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -8,4 +8,4 @@ apt -y install nginx
 rm /var/www/html/index.nginx-debian.html
 
 # Create the default page for the region
-wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-distribute-load-with-traffic-manager/master/index.$1.html -O /var/www/html/index.html
+wget https://raw.githubusercontent.com/rambo2/Test-Funcs/master/index.$1.html -O /var/www/html/index.html
