a614a0a3c6cc3fab6fa6d14b4bff503b220d0e5e
Rambo <ctony123@gmail.com>
2022-01-26 00:06:46 +1000

    Update setup-nginx2.sh

--- setup-nginx2.sh
+++ setup-nginx2.sh
@@ -24,9 +24,4 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx
-# Remove the default page
-rm /var/www/html/index.nginx-debian.html
-
-# Create the default page for the region
-wget https://raw.githubusercontent.com/rambo2/Test-Funcs/master/index.$1.html -O /var/www/html/index.html
 
